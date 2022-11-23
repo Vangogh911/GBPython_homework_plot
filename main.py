f516625a6c3fe5ff74e0fee56b739ba36fe93cee
master <frami@framin1991.com>
2022-11-23 21:21:57 +0500

    final commit

--- main.py
+++ main.py
@@ -12,34 +12,66 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 x = np.arange(-10, 10, 0.01)
-rise_flag = False
-func_rise = []
-func_fall = []
+f_rise_positive = []
+f_rise_negative = []
+f_fall_positive = []
+f_fall_negative = []
 
 
 def func(x):
-    function = -12*x**4 * math.sin(math.cos(x)) - 18*x**3 + 5*x**2 + 10*x - 30
+    function = -12 * x ** 4 * math.sin(math.cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30
     return function
 
 
 def fall_rise_ind(x):
-    global rise_flag
-    global func_fall
-    global func_rise
+    global f_fall_positive
+    global f_fall_negative
+    global f_rise_positive
+    global f_rise_negative
+    rise_flag = False
+    x_fall = []
+    x_rise = []
+    x_above_0 = []
+    x_below_0 = []
     buffer_index = 0
     for i in range(len(x) - 1):
         if func(x[i]) > func(x[i + 1]):
             if rise_flag:
-                func_rise.append(x[buffer_index: i])
+                x_fall.append(x[buffer_index: i])
                 buffer_index = i
                 rise_flag = False
         else:
             if not rise_flag:
-                func_fall.append(x[buffer_index: i])
+                x_rise.append(x[buffer_index: i])
                 buffer_index = i
                 rise_flag = True
+
+    for k in range(len(x_fall)):
+        storage_pos = []
+        storage_neg = []
+        for j in x_fall[k]:
+            if func(j) < 0:
+                storage_neg.append(j)
+            else:
+                storage_pos.append(j)
+        x_below_0.append(storage_neg)
+        x_fall[k] = storage_pos
+    for k in range(len(x_rise)):
+        storage_pos = []
+        storage_neg = []
+        for j in x_rise[k]:
+            if func(j) < 0:
+                storage_neg.append(j)
+            else:
+                storage_pos.append(j)
+        x_above_0.append(storage_neg)
+        x_rise[k] = storage_pos
+
+    f_fall_positive = x_fall
+    f_rise_positive = x_rise
+    f_fall_negative = x_below_0
+    f_rise_negative = x_above_0
 
 
 def change_func(x):
@@ -49,12 +81,18 @@ def change_func(x):
     plt.ylabel('Ось Y')
     plt.grid()
     plt.plot([x[0], x[-1]], [0, 0], ':g')
-    for i in func_fall:
+    for i in f_fall_positive:
         y = [func(j) for j in i]
         plt.plot(i, y, 'r')
-    for i in func_rise:
+    for i in f_fall_negative:
+        y = [func(j) for j in i]
+        plt.plot(i, y, '--r')
+    for i in f_rise_positive:
         y = [func(j) for j in i]
         plt.plot(i, y, 'b')
+    for i in f_rise_negative:
+        y = [func(j) for j in i]
+        plt.plot(i, y, '--b')
     plt.show()
 
 
